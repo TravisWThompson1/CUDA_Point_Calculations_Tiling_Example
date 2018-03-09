30a2c2061ba0bddf3088911c144b9086a8a4a434
Travis Thompson <Traviswthompson1@gmail.com>
2018-03-09 14:53:08 -0800

    init

---
+++ tiling_test.cpp
@@ -0,0 +1,62 @@
+//
+// Created by travis on 3/9/18.
+//
+
+
+
+
+
+
+
+
+int main(){
+
+    // Initialize tile size and number of tiles.
+    int WIDTH = 1024;
+    int HEIGHT = 1024;
+    int NUM_TILES = 1000;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
